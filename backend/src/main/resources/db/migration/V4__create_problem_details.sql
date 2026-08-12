f1da55a81ec18b943c4677a4778c53afb05870ca
karannchaudhari04 <karanchaudhari34804@gmail.com>
2026-08-12 13:30:53 +0530

    feat: initialize database schema with baseline Flyway migration script

--- backend/src/main/resources/db/migration/V4__create_problem_details.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE problem_details (
-    problem_id UUID PRIMARY KEY,
-    intuition TEXT NOT NULL,
-    approach TEXT NOT NULL,
-    code_snippet TEXT NOT NULL,
-    complexity_analysis TEXT,
-    what_i_learned TEXT,
-    CONSTRAINT fk_details_problem FOREIGN KEY (problem_id) REFERENCES problems(id) ON DELETE CASCADE
-);
